45e7978fd17940685ff9e90b493e518df3e7bd0e
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-09-21 10:36:12 +0200

    Update Program.cs

--- c#/Random password generator csharp (not form)/Random password generator csharp (not form)/Program.cs
+++ c#/Random password generator csharp (not form)/Random password generator csharp (not form)/Program.cs
@@ -12,12 +12,10 @@ namespace Random_password_generator_csharp__not_form_
         static void Main(string[] args)
         {
             char type = ' ';
-            char[] password;
-            string userName = Environment.UserName, path = $@"C:\Users\{userName}\AppData\Roaming\fs_psw_gen", passw_temp;
-            int lung_passw ,temp = 0;
-            Random rnd = new Random();
+            string password = "", userName = Environment.UserName, path = $@"C:\Users\{userName}\AppData\Roaming\fs_psw_gen";
+            int lung_passw = 0;
 
-            inizio:
+        inizio:
 
             Console.Write("How long do you want the password to be ");
             lung_passw = Convert.ToInt32(Console.ReadLine());
@@ -25,80 +23,20 @@ namespace Random_password_generator_csharp__not_form_
             Console.Write("You want only [c]haracters, [n]umber e characters, [s]ymbols numbers and characters ");
             type = Convert.ToChar(Console.ReadLine());
 
-            //Sets the lenght of the password array to the inserted password lenght
-            password = new char[lung_passw];
-
             Console.Clear();
 
             switch (type)
             {
                 case 'c':
-                    for (int i = 0; i < lung_passw; i++)
-                    {
-                        temp = rnd.Next(0, 2);
-                        if (temp == 1)
-                        {
-                            password[i] = (char)rnd.Next(65, 91);
-                        }
-                        else
-                        {
-                            password[i] = (char)rnd.Next(97, 123);
-                        }
-                    }
+                    password += charac(in lung_passw);
                     break;
 
                 case 'n':
-                    for (int i = 0; i < lung_passw; i++)
-                    {
-                        temp = rnd.Next(0, 3);
-                        if (temp == 0)
-                        {
-                            password[i] = (char)rnd.Next(65, 91);
-                        }
-                        else if (temp == 1)
-                        {
-                            password[i] = (char)rnd.Next(97, 123);
-                        }
-                        else
-                        {
-                            password[i] = (char)rnd.Next(48, 58);
-                        }
-                    }
+                    password += Number(in lung_passw);
                     break;
 
                 case 's':
-                    for (int i = 0; i < lung_passw; i++)
-                    {
-                        temp = rnd.Next(0, 7);
-                        if (temp == 0)
-                        {
-                            password[i] = (char)rnd.Next(65, 91);
-                        }
-                        else if (temp == 1)
-                        {
-                            password[i] = (char)rnd.Next(97, 123);
-                        }
-                        else if (temp == 2)
-                        {
-                            password[i] = (char)rnd.Next(48, 58);
-                        }
-                        else if (temp == 3)
-                        {
-                            password[i] = (char)rnd.Next(33, 48);
-                        }
-                        else if (temp == 4)
-                        {
-                            password[i] = (char)rnd.Next(58, 64);
-                        }
-                        else if (temp == 5)
-                        {
-                            password[i] = (char)rnd.Next(91, 97);
-                        }
-                        else if (temp == 6)
-                        {
-                            password[i] = (char)rnd.Next(123, 127);
-                        }
-                    }
+                    password += symbols(in lung_passw);
                     break;
 
                 default:
@@ -106,9 +44,6 @@ namespace Random_password_generator_csharp__not_form_
                     Console.WriteLine("Type not valid");
                     goto inizio;
             }
-
-            //Converts the arrays of char into a string
-            passw_temp = new string(password);
 
             //Saves the password
             if (!Directory.Exists(path))
@@ -120,7 +55,7 @@ namespace Random_password_generator_csharp__not_form_
                 using (StreamWriter writer = File.CreateText($@"{path}\psw.txt"))
                 {
                     //Saving the password
-                    writer.WriteLine(passw_temp);
+                    writer.WriteLine(password);
                 }
 
             }
@@ -129,14 +64,100 @@ namespace Random_password_generator_csharp__not_form_
                 //Saving the password to the file
                 using (StreamWriter writer = File.CreateText($@"{path}\psw.txt"))
                 {
-                    writer.WriteLine(passw_temp);
+                    writer.WriteLine(password);
                 }
             }
 
             //Prints the password
-            Console.Write(passw_temp);
+            Console.Write(password);
 
             Console.ReadKey();
+        }
+
+
+
+        static string charac(in int lung_passw)
+        {
+            Random rnd = new Random();
+            int temp;
+            string password = "";
+            for (int i = 0; i < lung_passw; i++)
+            {
+                temp = rnd.Next(0, 2);
+                if (temp == 1)
+                {
+                    password += (char)rnd.Next(65, 91);
+                }
+                else
+                {
+                    password += (char)rnd.Next(97, 123);
+                }
+            }
+            return $"{password}*";
+        }
+
+        static string Number(in int lung_passw)
+        {
+            Random rnd = new Random();
+            int temp;
+            string password = "";
+            for (int i = 0; i < lung_passw; i++)
+            {
+                temp = rnd.Next(0, 3);
+                if (temp == 0)
+                {
+                    password += (char)rnd.Next(65, 91);
+                }
+                else if (temp == 1)
+                {
+                    password += (char)rnd.Next(97, 123);
+                }
+                else
+                {
+                    password += (char)rnd.Next(48, 58);
+                }
+            }
+            return $"{password}*";
+        }
+
+        static string symbols(in int lung_passw)
+        {
+            Random rnd = new Random();
+            int temp;
+            string password = "";
+            for (int i = 0; i < lung_passw; i++)
+            {
+                temp = rnd.Next(0, 7);
+                if (temp == 0)
+                {
+                    password += (char)rnd.Next(65, 91);
+                }
+                else if (temp == 1)
+                {
+                    password += (char)rnd.Next(97, 123);
+                }
+                else if (temp == 2)
+                {
+                    password += (char)rnd.Next(48, 58);
+                }
+                else if (temp == 3)
+                {
+                    password += (char)rnd.Next(33, 48);
+                }
+                else if (temp == 4)
+                {
+                    password += (char)rnd.Next(58, 64);
+                }
+                else if (temp == 5)
+                {
+                    password += (char)rnd.Next(91, 97);
+                }
+                else if (temp == 6)
+                {
+                    password += (char)rnd.Next(123, 127);
+                }
+            }
+            return $"{password}*";
         }
     }
 }
